e444e176b97b4976de4ab621b68046e451d213f1
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:45:12 +0600

    add sizes

---
+++ lib/core/constants/sizes.dart
@@ -0,0 +1,6 @@
+const double betweenSpaceTwoElement = 12;
+const double padding = 10;
+const double boxMobilePadding = 2;
+const double boxTabPadding = 6;
+const double boxDesktopPadding = 6;
+const double itemBorderRadius = 5;
