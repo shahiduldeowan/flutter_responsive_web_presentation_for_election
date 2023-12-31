f3f6b8e95b44700cd7728c2eb3406bd2f949d99a
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:41:31 +0600

    Add urls

---
+++ lib/core/constants/api_urls.dart
@@ -0,0 +1,5 @@
+const String baseUrl = 'http://192.168.1.10:9009';
+
+const String resultUrl = '/voteweb/api/posting/election_result';
+const String socketUrl = '/voteweb/desoft/wsconnect'; // production
+const String socketResultSubscriberUrl = '/mobile/election_result';
