c7dabf4ac6f3b46e9be32001a9dc92466becfd96
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:49:45 +0600

    Add necessary constants and strings for project key, organization title, election year, president, board of directors, error messages, and socket connection status.

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,1 +1,15 @@
+const String projectKey = '100000';
+const String organizationTitle = 'Uttara Club Limited';
+const String deSoft = 'De-Soft';
+const String electionYear = 'Election for the year 2023-2024';
+const String president = 'President';
+const String boardOfDirector = 'Board of Directors';
 
+const String somethingWrong = 'Something went wrong!';
+const String uncontested = 'Uncontested';
+const String voteCounted = 'Counted';
+
+// Socket
+const String waitingToConnect = 'Waiting to connect...';
+const String connecting = 'Connecting...';
+const String ack = 'ack';
