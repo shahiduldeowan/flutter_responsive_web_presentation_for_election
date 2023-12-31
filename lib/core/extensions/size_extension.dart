04ff22c70cbc52cd75263c1337c48571e6bb4d6a
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 14:05:07 +0600

    Add responsive layout extension and size utilities.

--- lib/core/extensions/size_extension.dart
+++ lib/core/extensions/size_extension.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+import '../constants/sizes.dart' show boxDesktopPadding, boxMobilePadding, boxTabPadding;
+import '../utils/responsive_layout.dart';
+
+extension SizeExtension on BuildContext {
+  SizedBox get toHeight => SizedBox(
+        height: getSizes(
+          mobile: boxMobilePadding,
+          tab: boxTabPadding,
+          desktop: boxDesktopPadding,
+        ),
+      );
+
+  SizedBox get toWidth => SizedBox(
+        width: getSizes(
+          mobile: boxMobilePadding,
+          tab: boxTabPadding,
+          desktop: boxDesktopPadding,
+        ),
+      );
+
+  double? getSizes({double? mobile, double? tab, double? desktop}) {
+    if (ResponsiveLayout.isDesktop(this)) {
+      return desktop;
+    } else if (ResponsiveLayout.isTablet(this)) {
+      return tab;
+    } else {
+      return mobile;
+    }
+  }
+}
