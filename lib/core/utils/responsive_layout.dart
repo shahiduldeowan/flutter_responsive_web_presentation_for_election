a126f6b87bc063078638f5699d0ed35e6cb62f10
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:59:54 +0600

    Add ResponsiveLayout class and its dependencies.

--- lib/core/utils/responsive_layout.dart
+++ lib/core/utils/responsive_layout.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    super.key,
+    required this.mobile,
+    required this.tab,
+    required this.desktop,
+  });
+
+  final Widget mobile;
+  final Widget tab;
+  final Widget desktop;
+
+  // This isMobile, isTablet, isDesktop helep us later
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 650;
+
+  static bool isTablet(BuildContext context) => MediaQuery.of(context).size.width < 1100 && MediaQuery.of(context).size.width >= 650;
+
+  static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 1100;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      // If our width is more than 1100 then we consider it a desktop
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 1100) {
+          return desktop;
+        }
+        // If width it less then 1100 and more then 650 we consider it as tablet
+        else if (constraints.maxWidth >= 650) {
+          return tab;
+        }
+        // Or less then that we called it mobile
+        else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
