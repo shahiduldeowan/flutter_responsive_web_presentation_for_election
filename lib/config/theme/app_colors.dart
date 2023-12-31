44dfba1ea2b6aee9091c5ad4efdf6d6f6d5b4059
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:41:47 +0600

    add colors

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xff000000);
+const Color kCardColor = Color(0xff040404);
+const Color kBoxShadowColor = Color(0x59D9D9D9);
+
+const Color kHeightLightColor = Color(0xFFE3E91C);
+const Color kEliminateColor = Colors.orange;
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF9BCBFF),
+  onPrimary: Color(0xFF003256),
+  primaryContainer: Color(0xFF004A79),
+  onPrimaryContainer: Color(0xFFD0E4FF),
+  secondary: Color(0xFFBAC8DB),
+  onSecondary: Color(0xFF243240),
+  secondaryContainer: Color(0xFF3B4857),
+  onSecondaryContainer: Color(0xFFD6E4F7),
+  tertiary: Color(0xFFD5BEE5),
+  onTertiary: Color(0xFF3A2A48),
+  tertiaryContainer: Color(0xFF514060),
+  onTertiaryContainer: Color(0xFFF0DBFF),
+  error: Color(0xFFFFB4AB),
+  errorContainer: Color(0xFF93000A),
+  onError: Color(0xFF690005),
+  onErrorContainer: Color(0xFFFFDAD6),
+  background: kBackgroundColor,
+  // background: Color(0xFF1A1C1E),
+  onBackground: Color(0xFFE2E2E6),
+  surface: Color(0xFF1A1C1E),
+  onSurface: Color(0xFFE2E2E6),
+  surfaceVariant: Color(0xFF42474E),
+  onSurfaceVariant: Color(0xFFC2C7CF),
+  outline: Color(0xFF8C9199),
+  onInverseSurface: Color(0xFF1A1C1E),
+  inverseSurface: Color(0xFFE2E2E6),
+  inversePrimary: Color(0xFF00629F),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF9BCBFF),
+  outlineVariant: Color(0xFF42474E),
+  scrim: Color(0xFF000000),
+);
