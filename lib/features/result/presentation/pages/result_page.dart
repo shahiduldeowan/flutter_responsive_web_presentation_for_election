08089439ea66d559cfd697399265aac4a689045f
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:44:53 +0600

    Add routeName constant to the file

--- lib/features/result/presentation/pages/result_page.dart
+++ lib/features/result/presentation/pages/result_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ResultPage extends StatelessWidget {
   const ResultPage({super.key});
 
+  static const String routeName = '/';
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
