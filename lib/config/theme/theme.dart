73979e39c3de1a7ea393826b63bddb01c2d5ac9f
Shahidul Deowan <deowan@de-softbd.com>
2023-12-31 13:42:06 +0600

    add theme

---
+++ lib/config/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import './app_colors.dart' show darkColorScheme;
+
+ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: darkColorScheme,
+);
