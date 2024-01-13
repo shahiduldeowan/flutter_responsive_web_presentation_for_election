0857d0fcf302adf9995a339a863ec82949507bd3
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 17:03:13 +0600

    Update FetchResultUseCase implementation and imports.

--- lib/features/result/domain/usecases/fetch_result_usecase.dart
+++ lib/features/result/domain/usecases/fetch_result_usecase.dart
@@ -1,22 +1,15 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/core/error/failures.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/core/usecases/usecase.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/features/result/domain/entities/result_entity.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/features/result/domain/repositories/result_repository.dart';
+import '../../../../core/resources/data_state.dart';
+import '../../../../core/use_case/use_case.dart';
+import '../entities/publish_result_entity.dart';
+import '../repositories/result_repository.dart';
 
-// class FetchResultUseCase extends UseCase<ResultEntity, FetchResultParam> {
-//   final ResultRepository repository;
+class FetchResultUseCase extends UseCase<DataState<List<PublishResultEntity>>, void> {
+  final ResultRepository repository;
 
-//   FetchResultUseCase(this.repository);
+  FetchResultUseCase(this.repository);
 
-//   @override
-//   Future<Either<Failure, ResultEntity>> call(FetchResultParam params) async {
-//     return await repository.fetchResult(params: params);
-//   }
-// }
-
-// class FetchResultParam {
-//   String id;
-//   FetchResultParam({@required this.id});
-// }
+  @override
+  Future<DataState<List<PublishResultEntity>>> call({void param}) {
+    return repository.getResult();
+  }
+}
