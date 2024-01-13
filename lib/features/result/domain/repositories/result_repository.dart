33abd8194625a2b351ff5e2e7e82723e06ccb0bb
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 15:56:38 +0600

    Update ResultRepository and imports.

--- lib/features/result/domain/repositories/result_repository.dart
+++ lib/features/result/domain/repositories/result_repository.dart
@@ -1,8 +1,6 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/core/error/failures.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/features/result/domain/entities/result_entity.dart';
-// import 'package:flutter_responsive_web_presentation_for_election/features/result/domain/usecases/fetch_result_usecase.dart';
+import '../../../../core/resources/data_state.dart';
+import '../entities/publish_result_entity.dart';
 
-// abstract class ResultRepository {
-//   Future<Either<Failure, ResultEntity>> fetchResult({FetchResultParam params});
-// }
+abstract class ResultRepository {
+  Future<DataState<List<PublishResultEntity>>> getResult();
+}
