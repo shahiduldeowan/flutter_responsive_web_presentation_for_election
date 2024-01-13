39d1d72e8f436034bc1e2c1fbf71e573bbded284
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 14:47:04 +0600

    add publish_result_entity.dart files

---
+++ lib/features/result/domain/entities/publish_result_entity.dart
@@ -0,0 +1,52 @@
+import 'result_entity.dart';
+
+class PublishResultEntity extends ResultEntity {
+  const PublishResultEntity({
+    super.candidateSysKey,
+    super.candidateOrgKey,
+    super.groupKey,
+    super.candidateSl,
+    super.candidateName,
+    super.candidateImage,
+    super.group,
+    super.isCountable,
+    super.totalVote,
+    super.projectKey,
+    super.ballotIndex,
+    super.totalBallot,
+    super.remark,
+  });
+
+  @override
+  PublishResultEntity copyWith({
+    int? candidateSysKey,
+    String? candidateOrgKey,
+    int? groupKey,
+    String? candidateSl,
+    String? candidateName,
+    String? candidateImage,
+    String? group,
+    bool? isCountable,
+    int? totalVote,
+    int? projectKey,
+    int? ballotIndex,
+    int? totalBallot,
+    String? remark,
+  }) {
+    return PublishResultEntity(
+      candidateSysKey: candidateSysKey ?? this.candidateSysKey,
+      candidateOrgKey: candidateOrgKey ?? this.candidateOrgKey,
+      groupKey: groupKey ?? this.groupKey,
+      candidateSl: candidateSl ?? this.candidateSl,
+      candidateName: candidateName ?? this.candidateName,
+      candidateImage: candidateImage ?? this.candidateImage,
+      group: group ?? this.group,
+      isCountable: isCountable ?? this.isCountable,
+      totalVote: totalVote ?? this.totalVote,
+      projectKey: projectKey ?? this.projectKey,
+      ballotIndex: ballotIndex ?? this.ballotIndex,
+      totalBallot: totalBallot ?? this.totalBallot,
+      remark: remark ?? this.remark,
+    );
+  }
+}
