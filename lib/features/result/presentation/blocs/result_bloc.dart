eb73365799e20ecaa31e0b76f6ad8b2df520a64c
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 17:47:39 +0600

    updates ResultBloc to fetch and handle result data

--- lib/features/result/presentation/blocs/result_bloc.dart
+++ lib/features/result/presentation/blocs/result_bloc.dart
@@ -1,15 +1,40 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/resources/data_state.dart';
 import '../../domain/entities/publish_result_entity.dart';
+import '../../domain/usecases/fetch_result_usecase.dart';
 
 part 'result_event.dart';
 part 'result_state.dart';
 
 class ResultBloc extends Bloc<ResultEvent, ResultState> {
-  ResultBloc() : super(const StateResultLoading()) {
+  ResultBloc(this._fetchResultUseCase) : super(const StateResultLoading()) {
     on<EventFetchResult>(_fetchResult);
   }
 
-  Future<void> _fetchResult(EventFetchResult event, Emitter<ResultState> emit) async {}
+  final FetchResultUseCase _fetchResultUseCase;
+
+  Future<void> _fetchResult(EventFetchResult event, Emitter<ResultState> emit) async {
+    final resultSate = await _fetchResultUseCase();
+
+    if (resultSate is DataSuccess) {
+      _emitState(emit, resultSate.data!);
+    } else if (resultSate is DataFailed) {
+      emit(StateResultError(error: resultSate.error));
+    } else {
+      emit(const StateResultEmpty());
+    }
+  }
+
+  void _emitState(Emitter<ResultState> emit, List<PublishResultEntity> resultList) {
+    if (state is StateResultLoaded) {
+      final currentState = state as StateResultLoaded;
+      emit(currentState.copyWith(results: resultList));
+
+      return;
+    }
+
+    emit(StateResultLoaded(results: resultList));
+  }
 }
