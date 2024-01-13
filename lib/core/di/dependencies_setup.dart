bf6959876f81311df3ec15040f0c9227e0b1d674
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 17:46:43 +0600

    adds result_injector.dart import and call resultInjection function

--- lib/core/di/dependencies_setup.dart
+++ lib/core/di/dependencies_setup.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 
+import '../../features/result/di/result_injectort.dart' show resultInjection;
+
 final GetIt getIt = GetIt.instance;
 
 Future<void> dependenciesInjection() async {
   getIt.registerSingleton<Client>(Client());
+  resultInjection();
 }
