cd3860af23e9e88b5663272c6b0d923a52b56670
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 15:44:59 +0600

    added publish_result_group_entity.dart file

---
+++ lib/features/result/domain/entities/publish_result_group_entity.dart
@@ -0,0 +1,16 @@
+import './result_entity.dart';
+
+enum ResultGroup {
+  president('President'),
+  boardOfDirectory('Board Of Director'),
+  other('Other');
+
+  const ResultGroup(this._value);
+  final String _value;
+
+  String get get => _value;
+}
+
+mixin PublishResultGroupEntity on ResultEntity {
+  bool get isPresident => group?.contains(ResultGroup.president.get) ?? false;
+}
