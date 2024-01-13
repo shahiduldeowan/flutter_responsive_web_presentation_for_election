efe19067cd8f4c1d89e138cb3577c5e7b939bbb2
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 17:43:56 +0600

    Replaces the existing MaterialApp.router with a new MultiBlocProvider widget, Provides the ResultBloc and its associated event for fetching results

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import './config/theme/theme.dart';
 import './core/constants/strings.dart' show electionResults;
 import 'config/router/router.dart';
+import 'core/di/dependencies_setup.dart';
+import 'features/result/presentation/blocs/result_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: electionResults,
-      themeMode: ThemeMode.dark,
-      darkTheme: darkTheme,
-      routerConfig: MyRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<ResultBloc>()..add(const EventFetchResult()),
+        )
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: electionResults,
+        themeMode: ThemeMode.dark,
+        darkTheme: darkTheme,
+        routerConfig: MyRouter.router,
+      ),
     );
   }
 }
