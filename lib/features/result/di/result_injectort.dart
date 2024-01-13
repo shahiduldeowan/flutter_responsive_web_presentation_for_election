7e384ff27216294653d1673f077ea9183b7b7f2c
Shahidul Deowan <deowanshahidul@gmail.com>
2024-01-13 17:47:14 +0600

    adds dependency setup and result related classes

--- lib/features/result/di/result_injectort.dart
+++ lib/features/result/di/result_injectort.dart
@@ -1,3 +1,16 @@
 // Dependency injection configuration
 
-void resultInjection() {}
+import '../../../core/di/dependencies_setup.dart' show getIt;
+import '../data/data_sources/result_api_service.dart';
+import '../data/repositories/result_repository_impl.dart';
+import '../domain/repositories/result_repository.dart';
+import '../domain/usecases/fetch_result_usecase.dart';
+import '../presentation/blocs/result_bloc.dart';
+
+void resultInjection() {
+  getIt
+    ..registerLazySingleton<ResultApiService>(() => ResultApiService(getIt()))
+    ..registerLazySingleton<ResultRepository>(() => ResultRepositoryImpl(getIt()))
+    ..registerLazySingleton<FetchResultUseCase>(() => FetchResultUseCase(getIt()))
+    ..registerFactory<ResultBloc>(() => ResultBloc(getIt()));
+}
